e63aad532983f785f35fffc138d5f12f645e7e9e
mattw <matt@wholesalealgorithms.com>
2025-01-07 17:25:43 -0800

    Initial Commit

---
+++ Assets/CameraFeed.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using UnityEngine;
+#if UNITY_ANDROID
+using UnityEngine.Android;
+using UnityEngine.UI;
+#endif
+
+public class CameraFeed : MonoBehaviour
+{
+#if UNITY_IOS || UNITY_WEBGL
+    private bool CheckPermissionAndRaiseCallbackIfGranted(UserAuthorization authenticationType, Action authenticationGrantedAction)
+    {
+        if (Application.HasUserAuthorization(authenticationType))
+        {
+            if (authenticationGrantedAction != null)
+                authenticationGrantedAction();
+
+            return true;
+        }
+        return false;
+    }
+
+    private IEnumerator AskForPermissionIfRequired(UserAuthorization authenticationType, Action authenticationGrantedAction)
+    {
+        if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
+        {
+            yield return Application.RequestUserAuthorization(authenticationType);
+            if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
+                Debug.LogWarning($"Permission {authenticationType} Denied");
+        }
+    }
+#elif UNITY_ANDROID
+    private void PermissionCallbacksPermissionGranted(string permissionName)
+    {
+        StartCoroutine(DelayedCameraInitialization());
+    }
+
+    private IEnumerator DelayedCameraInitialization()
+    {
+        yield return null;
+        InitializeCamera();
+    }
+
+    private void PermissionCallbacksPermissionDenied(string permissionName)
+    {
+        Debug.LogWarning($"Permission {permissionName} Denied");
+    }
+
+    private void AskCameraPermission()
+    {
+        var callbacks = new PermissionCallbacks();
+        callbacks.PermissionDenied += PermissionCallbacksPermissionDenied;
+        callbacks.PermissionGranted += PermissionCallbacksPermissionGranted;
+        Permission.RequestUserPermission(Permission.Camera, callbacks);
+    }
+#endif
+
+    void Start()
+    {
+#if UNITY_IOS || UNITY_WEBGL
+        StartCoroutine(AskForPermissionIfRequired(UserAuthorization.WebCam, () => { InitializeCamera(); }));
+        return;
+#elif UNITY_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            AskCameraPermission();
+            return;
+        }
+#endif
+        InitializeCamera();
+    }
+
+    private void InitializeCamera()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        foreach (WebCamDevice device in devices)
+        {
+            Debug.Log(device.name);
+        }
+        Debug.Log("Creating WebCamTexture");
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Debug.Log("Getting RawImage");
+        RawImage image = GetComponent<RawImage>();
+        image.texture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
